951b4898d3a2bb89b8f6d8048a9f478b9fc0406d
Joshua Joppie <jshjpp@gmail.com>
2018-08-19 21:21:17 -0400

    Update inject.js
    Updated most of the script to actually function now. -- Script before accidentally had a break within the loop, causing script to end after first in array.

--- other/GooglePhotos/inject.js
+++ other/GooglePhotos/inject.js
@@ -1,16 +1,25 @@
- var evt = new MouseEvent("click", {
-        view: window,
-        bubbles: true,
-        cancelable: true,
-        clientX: 20,
-    });
 var aTags = document.getElementsByTagName("span");
 var searchText = "Save";
 var found;
 
 for (var i = 0; i < aTags.length; i++) {
   if (aTags[i].textContent == searchText) {
-    aTags[i].dispatchEvent(evt);
- 	break;
+	 triggerMostButtons(aTags[i]);
+  } else {
   }
 }
+
+function triggerMostButtons (buttonNode) {
+    triggerMouseEvent (buttonNode, "mouseover");
+    triggerMouseEvent (buttonNode, "mousedown");
+    triggerMouseEvent (buttonNode, "mouseup");
+    triggerMouseEvent (buttonNode, "click");
+}
+
+function triggerMouseEvent (node, eventType) {
+    var clickEvent = document.createEvent('MouseEvents');
+    clickEvent.initEvent (eventType, true, true);
+    node.dispatchEvent (clickEvent);
+}
+
+// https://stackoverflow.com/questions/15511381/normal-button-clicking-approaches-are-not-working-in-greasemonkey-script
